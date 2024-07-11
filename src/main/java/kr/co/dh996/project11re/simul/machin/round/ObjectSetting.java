cdc5192864d223ee42cc0d920235098db4db8006
dh996 <aaw987@naver.com>
2024-07-11 11:50:11 +0900

    임시

--- src/main/java/kr/co/dh996/project11re/simul/machin/round/ObjectSetting.java
+++ src/main/java/kr/co/dh996/project11re/simul/machin/round/ObjectSetting.java
@@ -1,5 +1,16 @@
 package kr.co.dh996.project11re.simul.machin.round;
 
+import org.springframework.stereotype.Component;
+
+import kr.co.dh996.project11re.simul.data.SimulMainObject;
+
+@Component
 public class ObjectSetting {
+	//오브젝트 관리 기능을 수행합니다.
+
+	public void setObj(SimulMainObject simulMO) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
